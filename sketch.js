b57156664e8fabb8712542785bd2effbb5df888a
sekulpizarro <62487398+sekulpizarro@users.noreply.github.com>
2020-12-08 18:10:39 -0300

    Archivo de pruebas para github pages

---
+++ sketch.js
@@ -0,0 +1,17 @@
+const video = document.getElementById('uservideo')
+faceapi.nets.tinyFaceDetector.loadFromUri('/develar/models')
+
+video.addEventListener('onload', ()  => {
+  video.addEventListener('play', () => {
+    const canvas = faceapi.createCanvasFromMedia(video)
+    document.body.append(canvas)
+    const displaySize = { width: video.width, height: video.height }
+    faceapi.matchDimensions(canvas, displaySize)
+    setInterval(async () => {
+      const detections = await faceapi.detectAllFaces(video, new faceapi.TinyFaceDetectorOptions())
+      const resizedDetections = faceapi.resizeResults(detections, displaySize)
+      canvas.getContext('2d').clearRect(0, 0, canvas.width, canvas.height)
+      faceapi.draw.drawDetections(canvas, resizedDetections)
+    }, 100)
+  })
+})
